f90d89bf25eb2999fda51dd7032d1dd271de13aa
THOM4S-XXIII <thomas-s93@hotmail.com>
2025-06-26 10:39:27 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// Oppgave 1: Array med norske byer
+const norskeByer = [
+  'Oslo',
+  'Bergen',
+  'Trondheim',
+  'Stavanger',
+  'Tromsø',
+  'Kristiansand',
+];
+
+// Oppgave 2: Array med seks forskjellige tall (minst 3 siffer)
+const tall = [123, 456, 789, 321, 654, 987];
+
+// Bonus: Array med objekter
+const people = [
+  { firstName: 'Ola', lastName: 'Nordmann', age: 28 },
+  { firstName: 'Kari', lastName: 'Hansen', age: 34 },
+  { firstName: 'Per', lastName: 'Olsen', age: 19 },
+];
